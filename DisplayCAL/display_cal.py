bc010dee33c2f55545f2b65e246fc8925952e9c3
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-06-05 16:31:47 +0100

    [#367] Fixed an unnecessary warning in `DisplayCAL.display_cal.MainFrame.profile_finish()`.

--- DisplayCAL/display_cal.py
+++ DisplayCAL/display_cal.py
@@ -11202,9 +11202,8 @@ class MainFrame(ReportFrame, BaseFrame, LUT3DMixin):
                     )
                     for key, name, _volume in gamuts:
                         try:
-                            gamut_coverage = float(
-                                profile.tags.meta.getvalue(f"GAMUT_coverage({key})")
-                            )
+                            gamut_coverage = profile.tags.meta.getvalue(f"GAMUT_coverage({key})")
+                            gamut_coverage = float(gamut_coverage) if gamut_coverage is not None else gamut_coverage
                         except (TypeError, ValueError):
                             traceback.print_exc()
                             gamut_coverage = None
